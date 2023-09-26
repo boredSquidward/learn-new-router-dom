a0bbe40f0fe7a91a57eaa10db32646a8bd453d10
hossein.k <rock.hkh19@yahoo.com>
2023-09-26 07:23:15 -0400

    children uncommented

--- src/main.jsx
+++ src/main.jsx
@@ -11,12 +11,12 @@ const router = createBrowserRouter([
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
-    // children: [
-    //   {
-    //     path: "/contacts/:contactId",
-    //     element: <Contact />,
-    //   },
-    // ],
+    children: [
+      {
+        path: "/contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
   },
 ]);
 
