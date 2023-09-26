28f371717bbf725c92977d383992673a1f6d109e
hossein.k <rock.hkh19@yahoo.com>
2023-09-26 07:11:08 -0400

    router added

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,21 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
+import Root from "./routes/root";
+import ErrorPage from "./error-page";
+import Contact from "./routes/contact";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
   },
 ]);
 
